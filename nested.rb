8ce1ce2edcf8b4610d555a7131a3d850cea4de62
Adam Finch <atyreefinch@gmail.com>
2019-08-05 18:43:29 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,12 +63,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -84,6 +78,11 @@ def adding_matz
           :languages => ["C"]
         }
      }
+# add the following information to the top level of programmer_hash
+# :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP", "C"]
+# }
 
     
 end
